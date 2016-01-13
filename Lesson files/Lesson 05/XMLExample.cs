a437d930189aa330c41bac7a8e1b3ca5320d5ac8
Neander Giljam <neandergamedev@gmail.com>
2016-01-13 11:34:18 +0100

    Presentation and lesson files lesson 05

---
+++ Lesson files/Lesson 05/XMLExample.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class XMLExample : MonoBehaviour {
+
+	private void Start() {
+		TextItem tmpTextItem = new TextItem (823, "Hallo daar mjensen!?");
+
+		XMLManager.DebugTextItemPlox (tmpTextItem);
+	}
+
+}
+
+public class TextItem {
+	public int index;
+	public string text;
+	
+	public TextItem() {}
+	public TextItem(int index, string text) {
+		this.index = index;
+		this.text = text;
+	}
+}
---
+++ Lesson files/Lesson 05/XMLManager.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class XMLManager {
+
+	public static void DebugTextItemPlox(TextItem textItem) {
+		Debug.Log (textItem.index + " | " + textItem.text);
+	}
+}
