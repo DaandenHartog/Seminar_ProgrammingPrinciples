ab15aa6e48219c44aa16c2e0e6d870cb62872d4c
Neander Giljam <neandergamedev@gmail.com>
2016-01-17 23:46:44 +0100

    Added project because zip is broken.

---
+++ Lesson files/Lesson 07/Lesson_07 - Debugging/Debugging/Assets/Scripts/Knight.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class Knight : MonoBehaviour {
+
+	private Orc orc;
+
+	private void Start() {
+		Debug.Log("Hallo ik ben een knight");
+
+		orc = GameObject.Find("Orc").GetComponent<Orc>();
+		if (orc == null) {
+			return;
+		}
+
+		Debug.Log("De distance tussen mij en de orc is: " + CheckDistance());
+	}
+
+	private float CheckDistance(Orc orc = null) {
+		float distance = Vector2.Distance(transform.position, orc.transform.position);
+		return distance;
+	}
+}
---
+++ Lesson files/Lesson 07/Lesson_07 - Debugging/Debugging/Assets/Scripts/Orc.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Orc : MonoBehaviour {
+
+	void Start () {
+		Debug.Log("Hallo ik ben een orc");
+	}
+}
